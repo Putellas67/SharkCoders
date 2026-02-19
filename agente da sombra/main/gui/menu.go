58bbb123788e9783946ffed839ef2ee753f7a5e4
Putellas67 <shark06686@sharkcoders.pt>
2026-02-19 19:53:49 +0000

    Add files via upload

---
+++ agente da sombra/main/gui/menu.go
@@ -0,0 +1,8 @@
+components {
+  id: "menu"
+  component: "/main/gui/menu.gui"
+  position {
+    x: 1.7957929
+    y: 0.72346276
+  }
+}
---
+++ agente da sombra/main/scripts/GameManager.lua
@@ -0,0 +1,5 @@
+local GameManager = {
+	current_level = "menu"
+}
+
+return GameManager
