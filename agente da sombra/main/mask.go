cafe3498e042291a2b36b3aabe359c7d6bf527db
Putellas67 <shark06686@sharkcoders.pt>
2026-03-05 20:00:42 +0000

    Add files via upload

---
+++ agente da sombra/main/mask.go
@@ -0,0 +1,30 @@
+embedded_components {
+  id: "mask"
+  type: "sprite"
+  data: "default_animation: \"\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/assets/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 9.0
+    y: 4.0
+  }
+  scale {
+    x: 3.664327
+    y: 3.500162
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"\"\n"
+  "mask: \"player\"\n"
+  ""
+}
